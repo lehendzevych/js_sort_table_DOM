e018596c23e4ec87cfd555168bd9d33266505869
Andrey Lehendzevych <andrey.lehendzevych@gmail.com>
2023-04-05 16:03:30 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const salaryToNumber = s => s.replace(/[,$]/g, '');
+
+table.tHead.addEventListener('click', (e) => {
+  if (!e.target.matches('th')) {
+    return;
+  }
+
+  const tableBody = table.tBodies[0];
+  const cellText = e.target.innerText;
+  const cellIndex = e.target.cellIndex;
+
+  const sortedRows = [...tableBody.rows].sort((a, b) => {
+    const aText = a.cells[cellIndex].innerText;
+    const bText = b.cells[cellIndex].innerText;
+
+    switch (cellText) {
+      case 'Name':
+      case 'Position':
+        return aText.localeCompare(bText);
+
+      case 'Age':
+        return +aText - +bText;
+
+      case 'Salary':
+        return salaryToNumber(aText) - salaryToNumber(bText);
+
+      default:
+        return 0;
+    }
+  });
+
+  tableBody.append(...sortedRows);
+});
